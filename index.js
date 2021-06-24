f296491f9c93de7f0c9ad4c75aca45c02028d2ea
Humphrey Curtis <humphreywcurtis@gmail.com>
2021-06-24 17:48:20 +0100

    Submitting arguments to database

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 // Initialise modal
-const modal = document.querySelector('.modal'); 
+const modal = document.querySelector(".modal"); 
 
 // Using materialise library to initialise modal 
 M.Modal.init(modal);
 
-// Initialise materialise select element 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('select');
-// });
-
 // Event listener on argument scheme selected 
-const selectElement = document.querySelector('.browser-default');
+const selectElement = document.querySelector(".browser-default");
 
 // Argument scheme selector
 const argumentSchemeSelected = document.getElementById("selectArgumentScheme"); 
@@ -22,9 +17,39 @@ selectElement.addEventListener('change', (event) => {
     console.log("Value selected is:");
     console.log(argumentSchemeSelected.value);
 
-    if (argumentSchemeSelected.value === "1") {
+    if (argumentSchemeSelected.value === "1") { // Will need to change to make better 
         actionSchemeForm.className = "show";
     } else {
         actionSchemeForm.className = "hide"; 
     }
+});
+
+// Change plus sign for argument 
+const form = document.querySelector("form"); 
+
+// Critical aciton scheme parameters 
+const casCircumstance = document.querySelector("#casCircumstance"); 
+const casAction = document.querySelector("#casAction"); 
+const casGoal = document.querySelector("#casGoal"); 
+const casValue = document.querySelector("#casAction"); 
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    console.log(casCircumstance.value);
+    console.log(casAction.value);
+    console.log(casGoal.value);
+    console.log(casValue.value);
+
+    db.collection("arguments").add({
+        scheme : "critical action scheme", 
+        circumstance : casCircumstance.value, 
+        action : casAction.value, 
+        goal : casGoal.value, 
+        value : casValue.value
+    }); 
+
+    var instance = M.Modal.getInstance(modal); 
+    instance.close(); 
+    
+    form.reset(); 
 });
